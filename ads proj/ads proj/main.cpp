69df63ce337d3de2eee73cfd130632ae6e599126
Youssef Salem <90195346+Youssef-salem@users.noreply.github.com>
2025-04-02 19:32:39 +0200

    link to Xcode

---
+++ ads proj/ads proj/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ads proj
+//
+//  Created by youssef salem on 4/2/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
